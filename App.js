fba24c34d0f12e9c3ffd1226f98e8054eabb060c
Khushidangi28 <Khushidangi220776@acropolis.in>
2024-12-10 15:33:19 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import Footer from './components/footer';
+import Home from './components/home';
+import car from './components/Car';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <>
+      <Counter />
+    </>
+  );
+}
+
+export default App;
